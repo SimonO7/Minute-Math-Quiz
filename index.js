828841c74c9e7f70c873a1cd0c314574f1f2a56e
SimonO7 <sdragonz461@gmail.com>
2022-08-13 23:16:08 -0700

    Display the results of each question in result box

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 let operand1 = 0;
 let operand2 = 0;
 
+const response = document.querySelector(".response");
+const score = document.querySelector(".score_display");
+const question = document.querySelector(".question");
+const result = document.querySelector(".result");
+
 function main()
 {
     document.addEventListener("keydown", checkAnswer);
@@ -10,28 +15,26 @@ function main()
 // Generate a question
 function makequestion()
 {
-    const response = document.querySelector(".response");
-    const question = document.querySelector(".question");
     operand1 = Math.floor(Math.random()*99)+1;
     operand2 = Math.floor(Math.random()*9)+1;
     question.innerHTML = String(operand1) + " + " + String(operand2) + " = ";
     response.value = "";
 }
 
-// Check answer
+// Check answer when Enter key is pressed
 function checkAnswer(event)
 {
-    const response = document.querySelector(".response");
     if (event.key == "Enter")
     {
         if ((operand1 + operand2) === Number(response.value))
         {
-            console.log("Correct!");
+            result.textContent = "Correct!";
+            score.textContent = Number(score.textContent) + 1
             makequestion();
         }
         else
         {
-            alert("incorrect");
+            result.textContent = "Incorrect!";
         }
         
     }
