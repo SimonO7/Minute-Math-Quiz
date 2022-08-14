9b0a67d00194bcbe15505bca90a97f3872d12e95
SimonO7 <sdragonz461@gmail.com>
2022-08-14 13:12:09 -0700

    Add documentation to functions

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// Javascript file for Hot Pencils
+
+// Define the global variables and select the fields that will need to be accessed by functions
 let operand1 = 0;
 let operand2 = 0;
 
@@ -6,13 +9,14 @@ const score = document.querySelector(".score_display");
 const question = document.querySelector(".question");
 const result = document.querySelector(".result");
 
+// Main function to load on page load
 function main()
 {
     document.addEventListener("keydown", checkAnswer);
     makequestion();
 }
 
-// Generate a question
+// Generate a random question
 function makequestion()
 {
     operand1 = Math.floor(Math.random()*99)+1;
@@ -26,6 +30,7 @@ function checkAnswer(event)
 {
     if (event.key == "Enter")
     {
+        // When the correct answer is entered
         if ((operand1 + operand2) === Number(response.value))
         {
             result.textContent = "Correct!";
@@ -34,19 +39,22 @@ function checkAnswer(event)
             makequestion();
             setTimeout(clearResults, 1000);
         }
+
+        // When an incorrect answer is entered
         else
         {
             response.value = "";
             result.style.color = "red"
             result.textContent = "Incorrect. Try again!";
         }
-        
     }
 }
 
+// Clear the results message
 function clearResults()
 {
     result.textContent = "";
 }
+
 
 main();
